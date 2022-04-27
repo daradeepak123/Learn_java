8f658b3ff2ab5f0b751bbfb261960109aae08d03
daradeepak123 <daradeepak123@outlook.com>
2022-04-27 20:44:15 +0530

    Data_Type

--- src/main/java/Data_types.java
+++ src/main/java/Data_types.java
@@ -10,8 +10,8 @@ public class Data_types {
 		
 		int a =10;
 		int b=30;
-		int c=a+b;
-		System.out.println("value of c is--->"+c);
+		a=a+b;
+		System.out.println("value of c is--->"+a);
 		
 		
 		System.out.println(hello);
