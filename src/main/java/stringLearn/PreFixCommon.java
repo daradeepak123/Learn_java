70ff5693f015e4c1a71d59adef44746cf2e15137
Deepak <102788188+daradeepak123@users.noreply.github.com>
2023-06-22 22:50:57 +0530

    Patterns and prefix

--- src/main/java/stringLearn/PreFixCommon.java
+++ src/main/java/stringLearn/PreFixCommon.java
@@ -1,5 +1,8 @@
 package stringLearn;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class PreFixCommon {
 	
 	
@@ -30,6 +33,63 @@ public class PreFixCommon {
 
 	
 	
+	public static String pre()
+	{
+		String strings[]= {"apple","application","apol"};
+		
+		
+		String firstString=strings[0];
+		String prefix="";
+		for(int i=0;i<firstString.length();i++)
+		{
+			char c=firstString.charAt(i);
+			for(int j=1;j<strings.length;j++)
+			{
+				if(i>=strings.length|| strings[j].charAt(i)!=c)
+				{
+					return prefix;
+				}
+			}
+			prefix+=c;
+			
+		}
+		return prefix;
+	}
+	
+	
+	
+	public static void common()
+	{
+		String str="common numbers";
+		
+		Map<Character,Integer> map=new HashMap<>();
+		
+		for(int i=0;i<str.length();i++)
+		{
+			if(map.containsKey(str.charAt(i)))
+			{
+				int a=map.get(str.charAt(i));
+				map.put(str.charAt(i), a+1);
+			}
+			else
+			{
+				map.put(str.charAt(i), 1);
+			}
+			
+		}
+		System.out.println(map);
+		
+		for(Map.Entry mm:map.entrySet())
+		{
+			System.out.println(mm.getKey() +" "+ mm.getValue());
+			
+			
+			
+		}
+		
+		
+	}
+	
 	
 
 	public static void main(String[] args) {
@@ -39,6 +99,9 @@ public class PreFixCommon {
 		String[] strings = {"apple", "application", "aptitude"};
 		String commonPrefix = findCommonPrefix(strings);
 		System.out.println("Common prefix: " + commonPrefix);
+		
+		System.out.println(pre());
+		common();
 
 	}
 
