1a41b9de9476f95098f319ae564484b7d320a100
Deepak <102788188+daradeepak123@users.noreply.github.com>
2023-12-05 23:49:08 +0530

    Modified Code for parallel execution

--- src/test/java/parallelExecutionLearn/GoogleTest.java
+++ src/test/java/parallelExecutionLearn/GoogleTest.java
@@ -5,14 +5,17 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class GoogleTest {
 	
 	@Test
 	public void hello() throws InterruptedException
 	{
-		System.setProperty("webdriver.chrome.driver", "C://chromedriver.exe");
+		//System.setProperty("webdriver.chrome.driver", "C://chromedriver.exe");
 		ChromeOptions options = new ChromeOptions();
 		options.addArguments("--remote-allow-origins=*");
+		WebDriverManager.chromedriver().setup();
 		WebDriver driver=new ChromeDriver(options);
 		
 		driver.manage().window().maximize();
@@ -20,7 +23,7 @@ public class GoogleTest {
 		
 		Thread.sleep(4000);
 		
-		driver.close();
+		driver.quit();
 	}
 
 }
--- src/test/java/parallelExecutionLearn/PHPTravel.java
+++ src/test/java/parallelExecutionLearn/PHPTravel.java
@@ -11,6 +11,8 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class PHPTravel {
 	@Test
 	public void dPicker() throws InterruptedException
@@ -19,9 +21,10 @@ public class PHPTravel {
 		
 		String Day="31";
 		
-		System.setProperty("webdriver.chrome.driver", "C://chromedriver.exe");
+		//System.setProperty("webdriver.chrome.driver", "C://chromedriver.exe");
 		ChromeOptions options = new ChromeOptions();
 		options.addArguments("--remote-allow-origins=*");
+		WebDriverManager.chromedriver().setup();
 		WebDriver driver=new ChromeDriver(options);
 		
 		driver.manage().window().maximize();
